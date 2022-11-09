d47206ead9362a2834c123f2fa49774c5b53191d
Jade Kim <JadeKim.dev@gmail.com>
2022-11-09 18:15:32 +0900

    [BE] init

---
+++ server/src/main/java/com/codestates/server/exception/ExceptionCode.java
@@ -0,0 +1,25 @@
+package com.codestates.server.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    USER_NOT_FOUND(404, "User not found"),
+    USER_EXISTS(409, "User exists"),
+    INVALID_USER_STATUS(400, "Invalid user status"),
+
+    ORDER_NOT_FOUND(404, "Order not found"),
+    CANNOT_CHANGE_ORDER(403, "Order can not change"),
+
+    NOT_IMPLEMENTATION(501, "Not Implementation");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
