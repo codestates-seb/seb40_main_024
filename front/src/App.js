3df1fafcf54fd0eff0351073a53d93b8a3f9810f
jioneee <qoxx3659@gmail.com>
2022-11-10 21:11:33 +0900

    [FE] feat: 컴포넌트 분리

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 // import MyPage from './Pages/MyPage';
 // import InfoPage from './Pages/InfoPage';
-import SubscriptionPage from './Pages/SubscriptionPage';
+// import SubscriptionPage from './Pages/SubscriptionPage';
 // import PaymentConfirmPage from './Pages/PaymentConfirmPage';
 // import PaymentPage from './Pages/PaymentPage';
-// import BoardContentPage from './Pages/BoardContentPage';
+import BoardContentPage from './Pages/BoardContentPage';
 
 function App() {
   return (
     <>
-      <SubscriptionPage />
+      <BoardContentPage />
     </>
   );
 }
