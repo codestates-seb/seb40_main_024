edefb81b7035ec491447b5460a0c0ff65d2e0020
HongSungHak <101723087+HongSungHak@users.noreply.github.com>
2022-11-10 14:29:11 +0900

    test : commit test

--- server/src/main/java/com/codestates/server/ServerApplication.java
+++ server/src/main/java/com/codestates/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.codestates.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // auditing 어노테이션 추가
 public class ServerApplication {
 
 	public static void main(String[] args) {
