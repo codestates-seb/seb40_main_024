6a66b3652f108ddbd92d8b36711357fa4b627e63
junny1995 <while0818@gmail.com>
2022-11-10 14:55:06 +0900

    feat: 현재자산확인 페이지 틀제작

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import MainHome from './Pages/MainHome';
+import Asset from './Pages/Asset';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<MainHome />}></Route>
+        <Route path="/" element={<MainHome />} />
+        <Route path="/asset" element={<Asset />} />
       </Routes>
     </div>
   );
---
+++ front/src/Pages/Asset.js
@@ -0,0 +1,68 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { Assetdata } from '../Component/AssetData';
+import styled from 'styled-components';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const MainPie = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  .pieheader {
+    padding: 10px;
+    color: #777;
+    background: rgba(237, 237, 237, 0.75);
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(20px);
+    -webkit-backdrop-filter: blur(20px);
+    border-radius: 10px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+  }
+`;
+
+const GraphPie = styled.div`
+  display: flex;
+  margin-top: 30px;
+  width: 400px;
+  background: rgba(237, 237, 237, 0.75);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  border-radius: 15px;
+`;
+
+const AssetExchange = styled.div`
+  color: #777;
+  width: 1000px;
+  height: 300px;
+  padding: 10px;
+  margin-top: 100px;
+  background: rgba(237, 237, 237, 0.75);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  .AssetExchangeBox {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+function Asset() {
+  return (
+    <MainPie>
+      <h2 className="pieheader">총 금액 확인하기</h2>
+      <GraphPie>
+        <Pie data={Assetdata} />
+      </GraphPie>
+      <AssetExchange>
+        <h2>현재 자산을 다른 자산과 비교해보세요!</h2>
+        <div className="AssetExchangeBox">
+          <span>현재보유현금 : 10,000 / USD로 환산시 : 7.28$</span>
+          <span>금</span>
+          <span>다이아몬드</span>
+          <span>주식</span>
+        </div>
+      </AssetExchange>
+    </MainPie>
+  );
+}
+
+export default Asset;
