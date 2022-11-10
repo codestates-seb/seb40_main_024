dd8ed311539d5e9a47f6d8d74567161ba79b71dc
junny1995 <while0818@gmail.com>
2022-11-10 12:15:10 +0900

    feat: 메인페이지 기본틀 제작

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import MainHome from './Pages/MainHome';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainHome />}></Route>
+      </Routes>
+    </div>
   );
 }
 
