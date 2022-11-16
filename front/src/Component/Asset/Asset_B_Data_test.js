e0cc60a28c7eff0eca1f744b6fbebd49e86f5bb5
dlagnsk2 <dlagnsk2@naver.com>
2022-11-16 16:00:35 +0900

    feat: 그래프 데이타 수정

--- front/src/Component/Asset/Asset_B_Data_test.js
+++ front/src/Component/Asset/Asset_B_Data_test.js
@@ -54,7 +54,7 @@ const data = [
   },
 ];
 
-export const AssetBdata = () => {
+export const AssetBdataTest = () => {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart
