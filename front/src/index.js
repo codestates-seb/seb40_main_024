171d925c678a2696b182cb9fe1594b917ba8d0c6
junny1995 <while0818@gmail.com>
2022-11-16 17:39:52 +0900

    test: redux 테스트 axios 코드 작성

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
 // import './index.css';
 
@@ -8,9 +10,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
